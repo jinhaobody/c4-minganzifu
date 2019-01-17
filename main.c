a5f7b5537a7a1d9ac0129335d57a7d0c4e419378
zhaojinhao <2481406659@qq.com>
2019-01-17 08:49:02 +0800

    -m框架

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+char arr[1024];
+int index = 0;
+int main()
+{
+
+    printf("jinruminganzifuguanlixitong");
+
+    while (1)
+    {
+        printf("1\n");
+        printf("2\n");
+        printf("3\n");
+        printf("4\n");
+        printf("5\n");
+        printf("qingshuruyaoxuanzedegongneng\n");
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+            printf("aaa\n");
+            char m;
+            scanf("%c", &m);
+            scanf("%c", &m);
+            arr[index] = m;
+            index++;
+            printf("aaa\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+    }
+    return 0;
+}
